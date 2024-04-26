c7b50413b92cdd6183b2e3a501e0a87fe4149d67
fatuma65 <namalefatuma28@gmail.com>
2024-04-26 17:18:11 +0300

    remove isAdmin User

--- airReserve/reserve/views.py
+++ airReserve/reserve/views.py
@@ -7,12 +7,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.exceptions import ValidationError
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.permissions import AllowAny
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
-from rest_framework.permissions import IsAdminUser
-
 
 # Create your views here.
 class BookingApiView(APIView):
@@ -129,8 +125,6 @@ class UserLogoutView(APIView):
 
 # admin shuld be able to create flights
 class AdminFlightsView(APIView):
-    permission_classes = [IsAdminUser]
-    authentication_classes = [TokenAuthentication]
 
     def post(self, request, *args, **kwargs):
         serializer = FlightSerializer(data=request.data)
